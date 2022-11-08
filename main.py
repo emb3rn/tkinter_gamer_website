8c38e02eea5471c755fc1e61b8d6b9e930f33446
Oskar Durys <embern@protonmail.com>
2022-11-08 09:17:34 +0000

    Version with macOS colors

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+#MACOS VERSION
 import tkinter as tk
+import tkinter.ttk as ttk
 
 window = tk.Tk("Website", None)
 window.configure(background="white")
@@ -50,15 +52,18 @@ for name in range(0,2):
 #ENTRY BOXES
 for entry in range(0, 2):
     entry = tk.Entry(main_frame,
-                        font=("Arial Regular", 24),
+                        font=("Arial Regular", 30),
                         width=20,
                         highlightbackground="black",
                         highlightthickness=1,
-                        border=0).place(x=305, y=250+entry*50)
+                        border=0,
+                        bg="white",
+                        fg="black").place(x=305, y=250+entry*50)
 
 #GENDER SELECTION
 l_gender = tk.Label(text="Gender?",
                     font=("Arial Regular", 14),
+                    fg="black",
                     bg=colors.background).place(x=95, y=375)
 
 rb_options = ["Male", "Female", "Prefer not to say"]
@@ -66,6 +71,7 @@ rb_options = ["Male", "Female", "Prefer not to say"]
 for option in range(0,3):
     radio_button = tk.Radiobutton(text=f"  {rb_options[option]}",
                             font=("Arial Regular", 14),
+                            fg="black",
                             bg=colors.background)
     radio_button.place(x=95, y=(420+option*40))
  
@@ -73,6 +79,7 @@ for option in range(0,3):
 #KIND OF GAMES
 l_games_play = tk.Label(text="Which games do you like to play?",
                     font=("Arial Regular", 14),
+                    fg="black",
                     bg=colors.background).place(x=500, y=375)
 
 cb_names = ["Quizzes", "Cards", "Dice", "Other"]
@@ -80,8 +87,25 @@ cb_names = ["Quizzes", "Cards", "Dice", "Other"]
 for name in range(0,4):
     checkbox = tk.Checkbutton(text=cb_names[name],
                             font=("Arial Regular", 14),
+                            fg="black",
                             bg=colors.background)
     checkbox.place(x=500, y=(415 + name*40))
+
+#AGE SELECTION
+
+l_games_play = tk.Label(text="Age? If over 30 choose 30.",
+                    font=("Arial Regular", 14),
+                    fg="black",
+                    bg=colors.background).place(x=95, y=575)
+
+s_age_slider = tk.Scale(main_frame,
+                        from_=0,
+                        to=30,
+                        font=("Arial Regular", 10),
+                        digits=True,
+                        fg="black",
+                        bg=colors.background,
+                        orient="horizontal").place(x=95, y=600)
 
 #QUIT/SUBMIT BUTTONS
 b_quit = tk.Button(main_frame,
@@ -90,7 +114,8 @@ b_quit = tk.Button(main_frame,
                    height=2,
                    width=12,
                    highlightbackground="#000000",
-                   highlightthickness=5).place(x=650, y=750)
+                   border=0,
+                   highlightthickness=1).place(x=650, y=750)
 
 b_submit = tk.Button(main_frame,
                    text="SUBMIT",
@@ -98,6 +123,9 @@ b_submit = tk.Button(main_frame,
                    height=2,
                    width=12,
                    highlightbackground="black",
-                   highlightthickness=5).place(x=775, y=750)
+                   border=0,
+                   highlightthickness=1).place(x=775, y=750)
+
+
 
 window.mainloop()
